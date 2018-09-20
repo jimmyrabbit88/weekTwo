5c2addd7fa1affe0d0eb4a82ac639a59734aad5e
jimmyrabbit88 <jason.dowling@students.ittralee.ie>
2018-09-20 15:14:18 +0100

    finished prob3

--- src/RoomQuote.java
+++ src/RoomQuote.java
@@ -10,16 +10,37 @@ public class RoomQuote {
         float cPSM = 0f;
         float totalCost = 0f;
 
-        getName();
-        setTitle();
+        name = getName();
+        len = Float.parseFloat(JOptionPane.showInputDialog(null, "Please enter the length of your room"));
+        wid = Float.parseFloat(JOptionPane.showInputDialog(null, "Please enter the width of your room"));
+        cPSM = Float.parseFloat(JOptionPane.showInputDialog(null, "Please enter the cost of carpet per sq meter"));
+        area = len*wid;
+        totalCost = doCalc(area, cPSM);
+        setTitle(name);
+        setValues(len, wid, area, cPSM, totalCost);
+        JOptionPane.showMessageDialog(null, quoteBox);
 
     }
 
     public static void setTitle(String name){
-        quoteBox.setText(String.format("%-40s", "Quotation for "+name));
+
+        quoteBox.setText(String.format("%-40s\n", "Quotation for "+name));
     }
 
     public static String getName(){
-        String name = 
+        String name = JOptionPane.showInputDialog(null, "Please enter your full Name", "Enter Name", 1);
+        return name;
+    }
+    public static float doCalc(float area, float cPSM){
+        float total;
+        total = area*cPSM;
+
+
+        return total;
+    }
+
+    public static void setValues(float len, float wid, float area, float cPSM, float tot){
+        quoteBox.append(String.format("%-40s%-15s\n%-40s%-15s\n%-40s%-15s\n%-40s%-15s\n%-40s%-15s\n", "Length of Room", len + "m", "Width of room", wid + "m",
+                                            "Area of Room :",area+"Msq", "Cost Per Meter Squared:", "Eur "+cPSM, "Total Cost", "Eur " + tot));
     }
 }
